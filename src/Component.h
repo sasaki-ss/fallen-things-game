c5f63f3646a33463cf5579635a649f5aa1526c26
Kuroneko <kaityou5555@gmail.com>
2023-05-04 01:04:46 +0900

    [update]Componentにシーン管理、キーボード入力を追加

--- src/Component.h
+++ src/Component.h
@@ -1,10 +1,16 @@
 #ifndef G_COMPONENT_H
 #define G_COMPONENT_H
 
+#include <memory>
 #include <random>
+
+#include "Keyboard.h"
+#include "SceneManager.h"
 
 struct Component {
 	std::mt19937 rand;
+	std::unique_ptr<Keyboard> keyboard;
+	std::unique_ptr<SceneManager> sceneMgr;
 };
 
 #endif // !G_COMPONENT_H
