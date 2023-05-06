ee3b96b85d8f8649bfb60b01b998c259c1cf1d52
Kuroneko <kaityou5555@gmail.com>
2023-05-06 23:02:40 +0900

    [add]Playerの追加

---
+++ src/Player.cpp
@@ -0,0 +1,45 @@
+#include "Player.h"
+
+#include <DxLib.h>
+#include "Component.h"
+#include "Keyboard.h"
+
+Player::Player(Component* comp) :
+	GameObject(comp),
+	_kb(nullptr) {
+
+}
+
+Player::~Player() {
+
+}
+
+bool Player::init() {
+	_pos = Vector2(600, 600-16);
+	_width = 48;
+	_height = 64;
+
+	_kb = _comp->getKeyboard();
+
+	return true;
+}
+
+void Player::end() {
+
+}
+
+void Player::update() {
+	if (_kb->getInputKey(KEY_INPUT_A) != 0) {
+		_pos.x -= 5.0f;
+	}
+
+	if (_kb->getInputKey(KEY_INPUT_D) != 0) {
+		_pos.x += 5.0f;
+	}
+}
+
+void Player::draw() {
+	DrawBoxAA(_pos.x - (_width / 2), _pos.y - (_height / 2),
+		_pos.x + (_width / 2), _pos.y + (_height / 2), GetColor(125, 125, 125), TRUE);
+	DrawCircle(_pos.x, _pos.y, 2, GetColor(255, 0, 0), TRUE);
+}
