dedc6e5a1fc46dafc2e5e7ad18c5c438c045adff
Kuroneko <kaityou5555@gmail.com>
2023-05-07 02:31:44 +0900

    [update]シーン生成機能をinline関数化

--- src/InGameCreate.cpp
+++
@@ -1,6 +0,0 @@
-#include "InGameCreate.h"
-#include "InGame.h"
-
-Scene* InGameCreate::createScene(Component* comp) {
-	return new InGame(comp);
-}
--- src/InGameCreate.h
+++ src/InGameCreate.h
@@ -2,12 +2,13 @@
 #define G_INGAMECREATE_H
 
 #include "ISceneCreate.h"
+#include "InGame.h"
 
 class Component;
 
 class InGameCreate final : public ISceneCreate {
 public:
-	Scene* createScene(Component* comp);
+	Scene* createScene(Component* comp){ return new InGame(comp); }
 };
 
 #endif // !G_INGAMECREATE_H
