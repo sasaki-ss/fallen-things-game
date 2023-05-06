17b2e355b92f077de2aab6bb59215999d0301d80
Kuroneko <kaityou5555@gmail.com>
2023-05-06 23:27:57 +0900

    [add]定数を追加

--- src/Define.h
+++ src/Define.h
@@ -1,9 +1,18 @@
 #ifndef G_DEFINE_H
 #define G_DEFINE_H
 
+#include <string>
+
 static const double FPS_STDTIME = 1000.0;
+static const int KEY_NUM = 256;
 
 static const int MAX_DROP_ITEM = 30;
 static const int GEN_ITEM_RATE = 50;
+
+static const std::string OBJ_KEYBOARD = "Keyboard";
+static const std::string OBJ_SCENEMANGER = "SceneManager";
+
+static const std::string OBJ_PLAYER = "Player";
+static const std::string OBJ_ITEMMGR = "ItemManager";
 
 #endif // !G_DEFINE_H
